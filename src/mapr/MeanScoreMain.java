9791c73aec058f56c2e63925ec3f00712e813608
yoseng28 <yoseng@163.com>
2021-10-20 00:13:34 +0800

    修改配置文件读取方法

--- src/mapr/MeanScoreMain.java
+++ src/mapr/MeanScoreMain.java
@@ -1,7 +1,8 @@
 package mapr;
 
 import java.io.IOException;
-import java.util.Properties;
+import java.net.URISyntaxException;
+import java.util.ResourceBundle;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -10,17 +11,20 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import tools.HDFSTools;
+
 public class MeanScoreMain {
 
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 
 		System.setProperty("HADOOP_USER_NAME", "root");
-		Properties p = new Properties();
-		String DATASETS_PATH = p.getProperty("DATASETS_PATH");
-		String RESULTS_PATH = p.getProperty("DATASETS_PATH");
 
-		String[] filePaths = { "/meanscore_math.txt", DATASETS_PATH + "/meanscore_python.txt",
-				DATASETS_PATH + "meanscore_java.txt", };
+		ResourceBundle resource = ResourceBundle.getBundle("conf");
+		String DATASETS_PATH = resource.getString("DATASETS_PATH");
+		String RESULTS_PATH = resource.getString("RESULTS_PATH");
+
+		String[] filePaths = { DATASETS_PATH + "/meanscore_math.txt", DATASETS_PATH + "/meanscore_python.txt",
+				DATASETS_PATH + "/meanscore_java.txt", };
 		String outputPath = RESULTS_PATH + "/meanscore";
 
 		Job job = Job.getInstance();
@@ -29,19 +33,27 @@ public class MeanScoreMain {
 		job.setMapperClass(MeanScoreMapper.class);
 		job.setCombinerClass(MeanScoreReducer.class);
 		job.setReducerClass(MeanScoreReducer.class);
-
-		// job.setMapOutputKeyClass(Text.class);
-		// job.setMapOutputValueClass(Text.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 
 		for (int i = 0; i < filePaths.length; i++) {
 			FileInputFormat.addInputPath(job, new Path(filePaths[i]));
 		}
+		try {
+			// 输出目录如果存在，删除
+			HDFSTools.deleteFileAndDir(outputPath);
+			HDFSTools.closeFileSystem();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
 		FileOutputFormat.setOutputPath(job, new Path(outputPath));
-		boolean result = job.waitForCompletion(true);
-		System.exit(result ? 0 : 1);
 
+		System.out.println("MR开始运行***********************************************");
+		long startTime = System.currentTimeMillis();
+		boolean result = job.waitForCompletion(true);
+		long endTime = System.currentTimeMillis();
+		System.out.println("MR运行时间：" + (endTime - startTime) / 1000 + "秒");
+		System.exit(result ? 0 : 1);
 	}
 
 }
